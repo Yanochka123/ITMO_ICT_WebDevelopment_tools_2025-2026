c543331d54e14eb58a6d12bfd3dde27a55d252e9
Yanochka123 <janorka2004@gmail.com>
2026-09-06 07:11:16 +0300

    added final code to practice 3

--- students/k3340/Norkina_Yaroslava/Lr1/time-management-app/connection.py
+++ students/k3340/Norkina_Yaroslava/Lr1/time-management-app/connection.py
@@ -5,11 +5,11 @@ from sqlmodel import SQLModel, Session, create_engine
 load_dotenv()
 
 # Получаем отдельные параметры
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "time_management")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 
 # Формируем строку подключения
 db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
